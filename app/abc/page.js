92b2d690048b3c3e7cf9aa8659542a6a9b7f3b4c
hge2ne <hge2ne@gmail.com>
2025-05-29 21:01:27 +0900

    라우팅 끝

---
+++ app/abc/page.js
@@ -0,0 +1,8 @@
+export default function abc() {
+    return(
+        <div>
+            <h1>abc페이지예요</h1>
+        </div>
+    )
+
+}
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import Link from "next/link"
 export default function Home() {
     let name ='park'
+
   return (
     <div>
-<h4 className="title">애플후레시</h4>
+        <h4 className="title">애플후레시</h4>
         <p className="title-sub">by dev {name}</p>
     </div>
   );
