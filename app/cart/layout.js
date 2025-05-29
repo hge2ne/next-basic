b154ae00a960d75e76efc8ac1e8d384ac0893023
hge2ne <hge2ne@gmail.com>
2025-05-29 21:48:58 +0900

    commit

--- app/cart/layout.js
+++ app/cart/layout.js
@@ -1,5 +1,6 @@
 export default function RootLayout({ children }) {
 return (
+
     <p>현대카드 무이자이벤트중</p>
 )
 }
--- app/list/page.js
+++ app/list/page.js
@@ -1,12 +1,17 @@
 export default function List() {
+    let 상품 =['Tomatoes','Pasta','Coconut']
+
     return (
         <div>
             <h4 className="title">상품목록</h4>
            <div className="food">
-               <h4>상품1 $40</h4>
+               <h4>상품[0]</h4>
            </div>
             <div className="food">
-                <h4>상품2 $40</h4>
+                <h4>상품[1] $40</h4>
+            </div>
+            <div className="food">
+                <h4>상품[2]</h4>
             </div>
         </div>
     );
