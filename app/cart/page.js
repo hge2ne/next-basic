70e6be874385b034c7751f87c3e7e1ac58085c43
hge2ne <hge2ne@gmail.com>
2025-05-30 17:16:43 +0900

    컴포넌트

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,23 @@
 export default function cart() {
-    return (
-        <div>
-            <h4 className="title">Cart</h4>
-            <div className="cart-item">
-                <p>상품명</p>
-                <p>$40</p>
-                <p>1개</p>
-            </div>
-            <div className="cart-item">
-                <p>상품명</p>
-                <p>$40</p>
-                <p>1개</p>
-            </div>
+    return (<div>
+        <h4 className="title">Cart</h4>
+        <div className="cart-item">
+            <p>상품명</p>
+            <p>$40</p>
+            <p>1개</p>
+
         </div>
-    )
+        <CartItem></CartItem>
+        <CartItem></CartItem>
+        <CartItem></CartItem>
+
+    </div>)
+}
+
+function CartItem() {
+    return (<div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+    </div>)
 }
