2b0c4646491e496d779b9325f4590abc63a6e63d
hge2ne <hge2ne@gmail.com>
2025-05-31 12:06:58 +0900

    useState 끝

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,26 +1,31 @@
-import { age, intro } from "./data.js"
+import {age, intro} from "./data.js"
+
 export default function Cart() {
-    let 장바구니 = ['Tomatoes', 'Pasta']
+    let 장바구니 = ['Tomatoes', 'Pasta']//DB 데이터 대용
     return (
         <div>
             <p>{intro}</p>
-        <h4 className="title">Cart</h4>
-        <div className="cart-item">
-            <p>{age}</p>
-            <p>$40</p>
-            <p>2개</p>
-        </div>
-        <CartItem></CartItem>
-        <CartItem></CartItem>
-        <CartItem></CartItem>
+            <h4 className="title">Cart</h4>
+            <div className="cart-item">
+                <p>상품명</p>
+                <p>$40</p>
+                <p>2개</p>
+            </div>
+            <CartItem item={장바구니[0]}></CartItem>
+            <CartItem item={장바구니[1]}></CartItem>
+            <Btn color="red"/>
+            <Btn color="blue"/>
 
-    </div>)
+        </div>)
 }
 
-function CartItem() {
+function CartItem(props) {
     return (<div className="cart-item">
-        <p>상품명</p>
+        <p>{props.item}</p>
         <p>$40</p>
         <p>1개</p>
     </div>)
+}
+function Btn(props) {
+    return <button style={{background:props.color}}>구매하기</button>
 }
--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,19 @@
+'use client'
 import Image from "next/image";
+import {useState} from "react";
 
 export default function List() {
     let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+    let [수량,수량변경]=useState(0);
     return (<div>
         <h4 className="title">상품목록</h4>
         {상품.map((a, i) => {
             return (<div className="food" key={i}>
                 <Image src={`/food${i}.png`} width={200} height={200}/>
                 <h4>{a}</h4>
+                <button onClick={()=>{수량변경(수량-1)}}>+</button>
+                <span>{수량}</span>
+                <button onClick={()=>{수량변경(수량+1)}}>+</button>
             </div>)
         })}
 
